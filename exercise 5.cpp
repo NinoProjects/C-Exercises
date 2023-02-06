c75e17c1b3d231058e5a993fa7ddde2c1dd6367f
NinoProjects <121447717+NinoProjects@users.noreply.github.com>
2023-02-07 00:33:03 +0400

    Add files via upload

---
+++ exercise 5.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int number;
+    cout << "sheiyvane ricxvi: ";
+    cin >> number;
+    
+    if (number > 0) { 
+        cout << "kvadratuli fesvi ricxvis: " << sqrt(number) ;
+        
+    } else {
+        cout << "sheiyvane dadebiti ricxvi";
+    }
+
+    return 0;
+}
